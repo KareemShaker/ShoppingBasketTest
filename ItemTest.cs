003cb0507e2c1b98538803dbdce08aafd406542d
Kareem Shaker <k_shaker@msn.com>
2017-07-17 01:50:28 +0100

    first

---
+++ ItemTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ShoppingBasketTest
+{
+    [TestClass]
+    public class TestItem
+    {
+        Item i2;
+       [TestInitialize]
+        public void setup()
+        {
+            i2=new Item(10, "Tissue", 1.00m);
+        }
+        [TestMethod]
+        public void TestCreatingAnItem()
+        {
+            Item i1 = new Item(10, "Milk", 1.60m);
+            Assert.AreEqual(i1.Code, 10);
+            Assert.AreEqual(i1.Name, "Milk");
+            Assert.AreEqual(i1.Price, 1.60m);
+
+        }
+
+        [TestMethod]
+        public void TestChangePrice()
+        {
+            //Item i1 = new Item(10, "Tissue", 1.00m);
+            i2.ChangePrice(1.05m);
+            Assert.AreEqual(i2.Price, 1.05m);
+        }
+        [TestMethod]
+        public void TestChangeName()
+        {
+            //Item i1 = new Item(10, "Tissue", 1.00m);
+            i2.ChangeName("TissueBox");
+            Assert.AreEqual(i2.Name, "TissueBox");
+        }
+        [TestMethod]
+        public void TestChangeCode()
+        {
+            //Item i1 = new Item(10, "Tissue", 1.00m);
+            i2.ChangeCode(101);
+            Assert.AreEqual(i2.Code, 101);
+        }
+    }
+}
